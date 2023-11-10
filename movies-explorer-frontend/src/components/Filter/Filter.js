80d7669f108696da5c574ef610209939462e8336
Dmitriy Bezrukov <dima1989o-z@yandex.ru>
2023-11-10 05:46:28 +0300

    21

--- movies-explorer-frontend/src/components/Filter/Filter.js
+++ movies-explorer-frontend/src/components/Filter/Filter.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import './Filter.css';
 
 const Filter = ({ isMovieFilter, onFilter }) => {
   return (
     <section className="filter" aria-label="Фильтр">
-      <input type="checkbox" id="checkbox" className="filter__checkbox" 
-              onChange={onFilter}
-              checked={isMovieFilter}/>
+      <input
+        type="checkbox"
+        id="checkbox"
+        className="filter__checkbox"
+        onChange={onFilter}
+        checked={isMovieFilter}
+      />
       <label htmlFor="checkbox" className="filter__label">
         Короткометражки
       </label>
